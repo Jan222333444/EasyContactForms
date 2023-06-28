b32bf82a5a08b5b8c015e7296fd40c86475f19a7
Jan Korb <jankorb@gmx.net>
2023-06-28 16:59:37 +0200

    Added support for HTML form submissions - added custom controller for plain HTML form submissions - added dto for redirect input

---
+++ src/main/java/org/easycontactforms/core/controller/HTMLContactFormController.java
@@ -0,0 +1,68 @@
+package org.easycontactforms.core.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.easycontactforms.core.dtos.HTMLContactFormDto;
+import org.easycontactforms.core.models.ContactForm;
+import org.easycontactforms.core.pluginloader.PluginStore;
+import org.easycontactforms.core.services.ContactFormService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/contact")
+public class HTMLContactFormController {
+
+    private final ContactFormService service;
+
+    @Autowired
+    public HTMLContactFormController(ContactFormService service) {
+        this.service = service;
+    }
+
+    /**
+     * Handles plain form posts without javascript used
+     * @param origin Server of website
+     * @param contactFormDto Form data in plain html post encoding
+     * @return proper redirect back to website
+     */
+    @CrossOrigin
+    @PostMapping
+    public String process(@RequestHeader(HttpHeaders.ORIGIN) String origin, HTMLContactFormDto contactFormDto) {
+        log.debug("[POST] Saving Contact Form");
+
+        // Processing plugin hook before contact form processing
+        for(String key : PluginStore.instance.plugins.keySet()){
+            PluginStore.instance.plugins.get(key).beforeContactFormProcessing(new org.easycontactforms.api.models.ContactFormDto(contactFormDto.getEmail(), contactFormDto.getName(), contactFormDto.getSubject(), contactFormDto.getMessage()));
+        }
+        // processes contact form
+        ContactForm contactForm = service.saveContactForm(contactFormDto);
+        log.debug("[POST] processed data excluding plugins");
+
+        // Executing plugin hook contactFormProcessed
+        for(String key : PluginStore.instance.plugins.keySet()){
+            PluginStore.instance.plugins.get(key).contactFormProcessed(new org.easycontactforms.api.models.ContactForm(contactForm.getId(), contactFormDto.getEmail(), contactFormDto.getName(), contactFormDto.getSubject(), contactFormDto.getMessage()));
+        }
+        log.debug("[POST] successfully processed contact form");
+
+        /*
+         * Redirect handling
+         */
+        if (contactFormDto.getRedirect() != null) {
+            log.warn(contactFormDto.getRedirect());
+            if(origin == null){
+                return "redirect:" + contactFormDto.getRedirect();
+            }
+            if (contactFormDto.getRedirect().contains(origin)) {
+                return "redirect:" + contactFormDto.getRedirect();
+            }
+            return "redirect:" + origin + contactFormDto.getRedirect();
+        }
+        return "redirect:" + origin;
+    }
+}
